9f2d06663c38683e2779899fc14436147a7a0ded
Jovan Yoshioka <jovanyoshioka@gmail.com>
2021-05-21 22:23:47 -0400

    Added Universal "Switch Form Tabs" Animation. Started on Login.

--- js/main.js
+++ js/main.js
@@ -33,3 +33,44 @@ function closeModal(modalNode)
   document.querySelector(".modalBackground").classList.remove("show");
   modalNode.classList.remove("show");
 }
+
+/*******************
+ * MULTI-STEP FORM *
+ *******************/
+/**
+ * Dynamically applies animation of form switching to next tab.
+ * Supports both "next" and "back".
+ * 
+ * CSS Requirements:
+ * -All Form Tabs:
+ *   transition: opacity 0.5s, transform 0.5s;
+ * -Initially Hidden Form Tabs:
+ *   opacity: 0.0;
+ *   transform: translateX(-100%);
+ *   pointer-events: none;
+ * 
+ * @param hideID ID of form tab to slide out of view.
+ * @param showID ID of form tab to slide into view.
+ */
+function switchFormTabs(hideID, showID)
+{
+  var hideTab = document.getElementById(hideID);
+  var showTab = document.getElementById(showID);
+
+  // Determine translation of tab to hide based on current position of tab to show.
+  // If showTab is to the [right/left], it will slide to the [left/right], and thus hideTab should slide to the [left/right].
+  var transformMatrix = window.getComputedStyle(showTab).getPropertyValue("transform");
+  var transformVals = transformMatrix.replace(/(matrix\(|\))/g, '').split(', ');
+  // "translateX" is index 4 of above transform values array.
+  var translation = -(transformVals[4]);
+
+  // Slide hideTab out of view.
+  hideTab.style.opacity = 0.0;
+  hideTab.style.transform = "translateX(" + translation + "px)";
+  hideTab.style.pointerEvents = "none";
+
+  // Slide showTab into view.
+  showTab.style.opacity = 1.0;
+  showTab.style.transform = "translateX(0)";
+  showTab.style.pointerEvents = "all";
+}
--- pages/login.php
+++ pages/login.php
@@ -25,10 +25,29 @@
   <body id="login">
     <div class="wrapper">
       <h1>Code a Story</h1>
-      <form action="../php/login.php" method="POST">
-        <input type="text" id="loginID" name="loginID" placeholder="Your ID"><br>
-        <input class="orangeBtn" type="submit" value="Login">
-      </form>
+      <div class="forms">
+        <!-- For user to select method of login, either as student or teacher -->
+        <div id="loginSelector">
+          <h2>Login as...</h2>
+          <button onclick="switchFormTabs('loginSelector', 'studentForm')" class="orangeBtn">Student</button>
+          <button onclick="switchFormTabs('loginSelector', 'teacherForm')" class="orangeBtn">Teacher</button>
+        </div>
+        <!-- Login as student form -->
+        <form id="studentForm" action="../php/login.php?type=0" method="POST">
+          <input type="text" id="loginID" name="loginID" placeholder="Your ID"><br>
+          <input class="orangeBtn" type="submit" value="Login">
+        </form>
+        <!-- Login as teacher form -->
+        <!-- Note: Web app admins use this form as well -->
+        <form id="teacherForm" action="../php/login.php" method="POST">
+          <input type="text" id="loginID" name="loginID" placeholder="Your ID"><br>
+          <input class="orangeBtn" type="submit" value="Login">
+        </form>
+      </div>
     </div>
+
+    <script>
+      
+    </script>
   </body>
 </html>
